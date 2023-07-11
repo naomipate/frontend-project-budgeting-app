fe74ef070569f925b7d0d202d609f52d590e3070
Naomi-Pate <patenaomi99@gmail.com>
2023-07-11 03:05:12 -0400

    added calculated total to the navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,34 @@
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function NavBar() {
+  const [budgetData, setBudgetData] = useState([]);
+  let totalWealth = [];
+
+  async function fetchTransactions() {
+    try {
+      let URL =
+        process.env.NODE_ENV === "production"
+          ? "https://backend-project-budgeting-app.onrender.com/transactions"
+          : "http://localhost:3001/transactions";
+      let result = await axios.get(URL);
+
+      setBudgetData(result.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  function handleTotalWealth() {
+    budgetData.map((item) => totalWealth.push(item.amount));
+    return totalWealth.reduce((acc, curr) => acc + curr, 0);
+  }
+
   return (
     <nav className="navbar navbar-expand-lg text-bg-success p-2 font-link">
       <div className="container-fluid">
@@ -22,6 +50,7 @@ export default function NavBar() {
             </Link>
             <div className="text-bg-success p-2 font-link">
               Calculator Total:
+              {handleTotalWealth().toFixed(2)}
             </div>
           </div>
         </div>
